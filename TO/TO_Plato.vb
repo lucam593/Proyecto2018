e7e65334283f0f50ec1b35b01686b8ad9c94d7b1
Luis Castro Muñoz <luca05-93@hotmail.com>
2018-06-20 01:05:13 -0600

    entity y  dao platos

--- TO/TO_Plato.vb
+++ TO/TO_Plato.vb
@@ -4,4 +4,16 @@
     Property Precio As Double
     Property Fotografia As String
     Property Habilitado As Boolean
+
+    Sub New()
+
+    End Sub
+
+    Sub New(nombre As String, desc As String, precio As Double, foto As String, hab As Boolean)
+        Me.Nombre = nombre
+        Me.Descripcion = desc
+        Me.Precio = precio
+        Me.Fotografia = foto
+        Me.Habilitado = hab
+    End Sub
 End Class
