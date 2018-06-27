875f00d2f7db3c7bcd448cf87e880f4ae11caa90
Necrotavo <gapalomo.u@gmail.com>
2018-06-26 21:47:18 -0600

    WS en UI

---
+++ UIWeb/Rest/IPlatosWS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BL;
+using System.ServiceModel.Web;
+
+namespace UIWeb.Rest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPlatosWS" in both code and config file together.
+    [ServiceContract]
+    public interface IPlatosWS
+    {
+        [OperationContract]
+        [WebGet(RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        List<BL_Plato> listaPlaots();
+
+        [OperationContract]
+        BL_Plato plato(Int16 cod);
+    }
+}
---
+++ UIWeb/Rest/PlatosWS.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BL;
+
+namespace UIWeb.Rest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PlatosWS" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PlatosWS.svc or PlatosWS.svc.cs at the Solution Explorer and start debugging.
+    public class PlatosWS : IPlatosWS
+    {
+        public List<BL_Plato> listaPlaots()
+        {
+            ManejadorPlatos platos = new ManejadorPlatos();
+            return platos.cargarPlatos();
+        }
+
+        public BL_Plato plato(short cod)
+        {
+            BL_Plato blPlato = new BL_Plato();
+            blPlato.cargarPlato(cod);
+            return blPlato;
+        }
+    }
+}
